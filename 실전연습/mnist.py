d17b18b6a8f03110bd220465176f71c1471a57fc
Andrew <escapeanaemia@gmail.com>
2019-01-03 14:57:50 +0900

    no message

---
+++ 실전연습/mnist.py
@@ -0,0 +1,9 @@
+import sys, os
+sys.path.append(os.pardir) #부모 디렉터리 파일을 가져올 수 있또록 설정
+from dataset.mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True,normalize=False)
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
